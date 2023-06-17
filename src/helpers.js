b56175dbc906f74bd439a1353e28cd9e825e558d
Emilx1g <baboumianemil@gmail.com>
2023-06-18 00:34:39 +0400

    hw3

--- src/helpers.js
+++ src/helpers.js
@@ -4,9 +4,3 @@ export const getYear = (age) => {
   console.log(age);
   return new Date().getFullYear() - age;
 };
-export const isEmpty = (value) => {
-  return !value;
-};
-export const hasMinWordCount = (word, count) => {
-  return word.length < count;
-};
